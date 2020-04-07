13f0dba9ea330027a0ab255159e27ffc3200ac7c
Christian <irgendwas@nix.de>
2020-04-07 19:31:38 +0200

    spiel des lebens erste Formen implementiert. Effizienz muss gesteigert werden!!

--- spielDesLebens.py
+++ spielDesLebens.py
@@ -1,15 +1,26 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QLabel, QHBoxLayout
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QLabel, QHBoxLayout, QVBoxLayout, QGridLayout
 from PyQt5.QtGui import QPainter, QColor, QFont, QBrush, QPen, QImage, QPainterPath, QPolygonF, QPixmap
 from PyQt5.QtCore import Qt, QEvent, QRect, QPointF, QPropertyAnimation, QTimer
 import numpy as np
 
 
-FENSTERBREITE = 100
-FENSTERHOEHE = 100
+"""
+Vier Grundlegen:
+1. Eine tote Zelle mit genau drei lebenden Nachbarn wird in der Folgegeneration neu geboren.
+2. Lebende Zellen mit weniger als zwei lebenden Nachbarn sterben in der Folgegeneration an Einsamkeit.
+3. Eine lebende Zelle mit zwei oder drei lebenden Nachbarn bleibt in der Folgegeneration am Leben.
+4. Lebende Zellen mit mehr als drei lebenden Nachbarn sterben in der Folgegeneration an Überbevölkerung.
+"""
 
-WHITE = [255, 255, 255]
-BLACK = [0, 0, 0]
+
+FENSTERBREITE = 700
+FENSTERHOEHE = 700
+ANZAHLSPALTEN = 100
+ANZAHLREIHEN = 100
+
+LEBEND = [255, 255, 255]
+TOT = [0, 0, 0]
 
 
 class Window(QWidget):
@@ -18,63 +29,171 @@ class Window(QWidget):
         super().__init__()
 
         self.wW, self.wH = FENSTERBREITE, FENSTERHOEHE
-        self.img = QImage(self.wW, self.wH, QImage.Format_RGBA8888)
-        self.img.fill(Qt.white)
-        self.img.setPixelColor(20, 50, Qt.black)
+        self.spalten, self.reihen = ANZAHLSPALTEN, ANZAHLREIHEN
+        self.keyPressEvent = self.fn
+
+        # Feld erstellen
+        self.data = np.zeros((self.wW, self.wH, 3)).astype(np.uint8)
         self.field = QLabel()
-        self.field.setPixmap(QPixmap.fromImage(self.img))
+        self.bildAktualisieren()
 
-        self.next = QPushButton("Next")
-        self.next.clicked.connect(self.machWas)
+        # Buttons
+        self.button1 = QPushButton("kleiner Test")
+        self.button1.clicked.connect(self.machWas)
+        self.button2 = QPushButton("Testmuster erstellen")
+        self.button2.clicked.connect(self.anfangErstellen)
+        self.button3 = QPushButton("Konvertieren")
+        self.button3.clicked.connect(self.farbenKonvertieren)
+        self.button4 = QPushButton("richtig berechnen")
+        self.button4.clicked.connect(self.berechneNaechsteDaten)
 
-        self.layout = QHBoxLayout()
-        self.layout.addWidget(self.field)
-        self.layout.addWidget(self.next)
+        # Positionen festlegen
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.field, 0, 0, 0, 1)
+        self.layout.addWidget(self.button1, 0, 1)
+        self.layout.addWidget(self.button2, 1, 1)
+        self.layout.addWidget(self.button3, 2, 1)
+        self.layout.addWidget(self.button4, 3, 1)
+        #self.layout.addWidget(QPushButton("55"), 4, 1)
+
 
         self.setLayout(self.layout)
-
-        matrix = np.zeros(shape = (20,20), dtype=np.int8)
-        # print(matrix)
+        self.show()
 
 
+    def fn(self, e):
+        if e.key() == Qt.Key_Q:
+            self.figurGleiter()
+        if e.key() == Qt.Key_W:
+            self.figurSpiegelU()
 
-
-
-        width = 100
-        height = 100
-        data = np.random.randint(0, 256, size=(self.wW, self.wH, 3)).astype(np.uint8)
-        data = np.random.randint(0, 256, size=(self.wW, self.wH, 3)).astype(np.uint8)
-
-        data = np.array(data)
-
-        data = np.zeros((self.wW, self.wH, 3)).astype(np.uint8)
-
-        data[1][2] = WHITE
-
-        print(data[0])
-
-
-        img = QImage(self.wW, self.wH, QImage.Format_RGB32)
-        for x in range(self.wW):
-            for y in range(self.wH):
-                img.setPixel(x, y, QColor(*data[x][y]).rgb())
+    def bildAktualisieren(self):
+        img = QImage(self.spalten, self.reihen, QImage.Format_RGB32)
+        for x in range(self.spalten):
+            for y in range(self.reihen):
+                img.setPixel(x, y, QColor(*self.data[x][y]).rgb())
 
         pix = QPixmap.fromImage(img)
-        pix = QPixmap.scaledToWidth(pix, 700)
-
-
+        pix = QPixmap.scaledToWidth(pix, self.wW)
         self.field.setPixmap(pix)
-
-
-
-
         self.show()
 
 
     def machWas(self):
         print("ggg")
-        self.img.setPixelColor(25, 50, Qt.black)
-        self.field.setPixmap(QPixmap.fromImage(self.img))
+
+        self.data[2][3] = LEBEND
+        self.data[2][30] = LEBEND
+        self.data[2][32] = LEBEND
+        self.bildAktualisieren()
+
+
+    def anfangErstellen(self):
+        for i in range(ANZAHLSPALTEN // 3, int(ANZAHLSPALTEN * (2/3)), 5):
+            for j in range(ANZAHLREIHEN // 3, int(ANZAHLREIHEN * (2/3))):
+                self.data[i][j] = LEBEND
+        self.bildAktualisieren()
+
+    def farbenKonvertieren(self):
+        for i in range(ANZAHLSPALTEN):
+            for j in range(ANZAHLREIHEN):
+                if self.data[i][j][0] == 255:
+                    self.data[i][j] = TOT
+                elif self.data[i][j][0] == 0:
+                    self.data[i][j] = LEBEND
+
+        self.bildAktualisieren()
+
+    def berechneNaechsteDaten(self):
+        belebendeZellen = set()
+        sterbendeZellen = set()
+        # Rausfinden welche Zellen geaendert werden muessen
+        for i in range(1, ANZAHLSPALTEN-1):
+            for j in range(1, ANZAHLREIHEN-1):
+                lebendeNachbarn = self.anzahlLebendeNachbarnBerechnen(i, j)
+
+                # Zelle wird wiederbelebt, wenn genau 3 lebende Nachbarn
+                if self.data[i][j][0] == 0:
+                    if lebendeNachbarn == 3:
+                        belebendeZellen.add((i, j))
+                        #self.data[i][j] = LEBEND
+
+                # Zelle überlebt nur, wenn genau 1 oder 2 Nachbarn leben
+                else:
+                    if not (lebendeNachbarn == 2 or lebendeNachbarn == 3):
+                        sterbendeZellen.add((i, j))
+                        #self.data[i][j] = TOT
+
+        # betreffende Zellen aendern
+        for zelleB in belebendeZellen:
+            self.data[zelleB[0]][zelleB[1]] = LEBEND
+        for zelleT in sterbendeZellen:
+            self.data[zelleT[0]][zelleT[1]] = TOT
+
+
+        self.bildAktualisieren()
+
+
+
+    def anzahlLebendeNachbarnBerechnen(self, x, y):
+        anzahlLebendeNachbarn = 0
+        if self.data[x-1][y-1][0] == 255:
+            anzahlLebendeNachbarn += 1
+        if self.data[x][y-1][0] == 255:
+            anzahlLebendeNachbarn += 1
+        if self.data[x+1][y-1][0] == 255:
+            anzahlLebendeNachbarn += 1
+
+        if self.data[x-1][y][0] == 255:
+            anzahlLebendeNachbarn += 1
+        if self.data[x+1][y][0] == 255:
+            anzahlLebendeNachbarn += 1
+
+        if self.data[x-1][y+1][0] == 255:
+            anzahlLebendeNachbarn += 1
+        if self.data[x][y+1][0] == 255:
+            anzahlLebendeNachbarn += 1
+        if self.data[x+1][y+1][0] == 255:
+            anzahlLebendeNachbarn += 1
+
+        return anzahlLebendeNachbarn
+
+
+    def figurGleiter(self):
+        startX = ANZAHLSPALTEN // 2
+        startY = ANZAHLREIHEN // 2
+
+        self.data[startX][startY-1] = LEBEND
+        self.data[startX+1][startY] = LEBEND
+        self.data[startX-1][startY+1] = LEBEND
+        self.data[startX][startY+1] = LEBEND
+        self.data[startX+1][startY+1] = LEBEND
+
+        self.bildAktualisieren()
+
+    def figurSpiegelU(self):
+        startX = ANZAHLSPALTEN // 2
+        startY = ANZAHLREIHEN // 2
+
+        # obere Haelfte
+        self.data[startX-1][startY-3] = LEBEND
+        self.data[startX][startY-3] = LEBEND
+        self.data[startX+1][startY-3] = LEBEND
+        self.data[startX-1][startY-2] = LEBEND
+        self.data[startX+1][startY-2] = LEBEND
+        self.data[startX-1][startY-1] = LEBEND
+        self.data[startX+1][startY-1] = LEBEND
+
+        # untere Haelfte
+        self.data[startX-1][startY+1] = LEBEND
+        self.data[startX+1][startY+1] = LEBEND
+        self.data[startX-1][startY+2] = LEBEND
+        self.data[startX+1][startY+2] = LEBEND
+        self.data[startX-1][startY+3] = LEBEND
+        self.data[startX][startY+3] = LEBEND
+        self.data[startX+1][startY+3] = LEBEND
+
+        self.bildAktualisieren()
 
 
 
