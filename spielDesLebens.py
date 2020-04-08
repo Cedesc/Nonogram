7259c2cff420bf5766c74154042bfc16dd31e328
Christian <irgendwas@nix.de>
2020-04-08 12:52:16 +0200

    spiel des lebens effizienz durch alleiniges betrachten der weißen felder plus umfeld. Staerkerer Einsatz der lebendenZellen Liste und Timer muss noch hinzuegefuegt werden

--- spielDesLebens.py
+++ spielDesLebens.py
@@ -16,8 +16,8 @@ Vier Grundlegen:
 
 FENSTERBREITE = 700
 FENSTERHOEHE = 700
-ANZAHLSPALTEN = 100
-ANZAHLREIHEN = 100
+ANZAHLSPALTEN = 500
+ANZAHLREIHEN = 500
 
 LEBEND = [255, 255, 255]
 TOT = [0, 0, 0]
@@ -31,6 +31,7 @@ class Window(QWidget):
         self.wW, self.wH = FENSTERBREITE, FENSTERHOEHE
         self.spalten, self.reihen = ANZAHLSPALTEN, ANZAHLREIHEN
         self.keyPressEvent = self.fn
+        self.lebendeZellenListe = set()
 
         # Feld erstellen
         self.data = np.zeros((self.wW, self.wH, 3)).astype(np.uint8)
@@ -46,6 +47,8 @@ class Window(QWidget):
         self.button3.clicked.connect(self.farbenKonvertieren)
         self.button4 = QPushButton("richtig berechnen")
         self.button4.clicked.connect(self.berechneNaechsteDaten)
+        self.button5 = QPushButton("richtiger berechnen?")
+        self.button5.clicked.connect(self.berechneNaechsteDatenV2)
 
         # Positionen festlegen
         self.layout = QGridLayout()
@@ -54,7 +57,7 @@ class Window(QWidget):
         self.layout.addWidget(self.button2, 1, 1)
         self.layout.addWidget(self.button3, 2, 1)
         self.layout.addWidget(self.button4, 3, 1)
-        #self.layout.addWidget(QPushButton("55"), 4, 1)
+        self.layout.addWidget(self.button5, 4, 1)
 
 
         self.setLayout(self.layout)
@@ -118,7 +121,7 @@ class Window(QWidget):
                         belebendeZellen.add((i, j))
                         #self.data[i][j] = LEBEND
 
-                # Zelle überlebt nur, wenn genau 1 oder 2 Nachbarn leben
+                # Zelle überlebt nur, wenn genau 2 oder 3 Nachbarn leben
                 else:
                     if not (lebendeNachbarn == 2 or lebendeNachbarn == 3):
                         sterbendeZellen.add((i, j))
@@ -169,6 +172,12 @@ class Window(QWidget):
         self.data[startX][startY+1] = LEBEND
         self.data[startX+1][startY+1] = LEBEND
 
+        self.lebendeZellenListe.add((startX, startY-1))
+        self.lebendeZellenListe.add((startX+1, startY))
+        self.lebendeZellenListe.add((startX-1, startY+1))
+        self.lebendeZellenListe.add((startX, startY+1))
+        self.lebendeZellenListe.add((startX+1, startY+1))
+
         self.bildAktualisieren()
 
     def figurSpiegelU(self):
@@ -193,10 +202,74 @@ class Window(QWidget):
         self.data[startX][startY+3] = LEBEND
         self.data[startX+1][startY+3] = LEBEND
 
+
+        #obere Haelfte
+        self.lebendeZellenListe.add((startX-1, startY-3))
+        self.lebendeZellenListe.add((startX, startY-3))
+        self.lebendeZellenListe.add((startX+1, startY-3))
+        self.lebendeZellenListe.add((startX-1, startY-2))
+        self.lebendeZellenListe.add((startX+1, startY-2))
+        self.lebendeZellenListe.add((startX-1, startY-1))
+        self.lebendeZellenListe.add((startX+1, startY-1))
+
+
+        # untere Haelfte
+        self.lebendeZellenListe.add((startX-1, startY+1))
+        self.lebendeZellenListe.add((startX+1, startY+1))
+        self.lebendeZellenListe.add((startX-1, startY+2))
+        self.lebendeZellenListe.add((startX+1, startY+2))
+        self.lebendeZellenListe.add((startX-1, startY+3))
+        self.lebendeZellenListe.add((startX, startY+3))
+        self.lebendeZellenListe.add((startX+1, startY+3))
+
+
+
         self.bildAktualisieren()
 
 
+    def berechneNaechsteDatenV2(self):
+        belebendeZellen = set()
+        sterbendeZellen = set()
+        zuUeberpruefendeUmliegendeZellen = set()
 
+
+        for koordinaten in self.lebendeZellenListe:
+            lebendeNachbarn = self.anzahlLebendeNachbarnBerechnen(koordinaten[0], koordinaten[1])
+
+            # Zelle überlebt nur, wenn genau 2 oder 3 Nachbarn leben
+            if not (lebendeNachbarn == 2 or lebendeNachbarn == 3):
+                sterbendeZellen.add((koordinaten[0], koordinaten[1]))
+
+            # umliegende Zellen durch moegliche Wiederbelebungen betrachten
+            zuUeberpruefendeUmliegendeZellen.add((koordinaten[0]-1, koordinaten[1]-1))
+            zuUeberpruefendeUmliegendeZellen.add((koordinaten[0], koordinaten[1]-1))
+            zuUeberpruefendeUmliegendeZellen.add((koordinaten[0]+1, koordinaten[1]-1))
+            zuUeberpruefendeUmliegendeZellen.add((koordinaten[0]-1, koordinaten[1]))
+            zuUeberpruefendeUmliegendeZellen.add((koordinaten[0]+1, koordinaten[1]))
+            zuUeberpruefendeUmliegendeZellen.add((koordinaten[0]-1, koordinaten[1]+1))
+            zuUeberpruefendeUmliegendeZellen.add((koordinaten[0], koordinaten[1]+1))
+            zuUeberpruefendeUmliegendeZellen.add((koordinaten[0]+1, koordinaten[1]+1))
+
+        for koordinatenU in zuUeberpruefendeUmliegendeZellen:
+            if self.data[koordinatenU[0]][koordinatenU[1]][0] == 0:
+                # Zelle wird wiederbelebt, wenn genau 3 lebende Nachbarn
+                if self.anzahlLebendeNachbarnBerechnen(koordinatenU[0], koordinatenU[1]) == 3:
+                    belebendeZellen.add((koordinatenU[0], koordinatenU[1]))
+
+
+        # betreffende Zellen aendern
+        for zelleB in belebendeZellen:
+            self.data[zelleB[0]][zelleB[1]] = LEBEND
+            self.lebendeZellenListe.add((zelleB[0], zelleB[1]))
+        for zelleT in sterbendeZellen:
+            self.data[zelleT[0]][zelleT[1]] = TOT
+            self.lebendeZellenListe.remove((zelleT[0], zelleT[1]))
+
+        self.bildAktualisieren()
+
+
+    def lebendeZellenListeAktualisieren(self):
+        pass
 
 
 
